a2f14feee98b015dafd625e31681543450dfbb35
mknuel <your_email_address@example.com>
2025-06-17 21:03:08 +0100

    base setup

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#39a7c1",
+				"primary-light": "#e7f1f4",
+				background: "#f3f4f6",
+				"background-light": "#f9fafb",
+				"gray-900": "#191919",
+			},
+			fontFamily: {
+				myfont: ["Helvetica", "sans-serif"],
+			},
+		},
+	},
+	plugins: [],
+};
